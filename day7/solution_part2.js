621ca88b1d602e9679670da95ad04c55fa23ca74
Christopher Waite <chrismwaite@gmail.com>
2017-12-12 22:11:20 +0000

    A few notes on Day 7 Part 2

---
+++ day7/solution_part2.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const input_path = "input.txt";
+
+(function() {
+  let tower = [];
+  let root_node = null;
+
+  fs.readFile(input_path, (err, data) => {
+    if(err) throw err;
+    //console.log(data.toString());
+    data.toString().split("\n").forEach(function(row) {
+      //console.log(row + "-----");
+      row_pieces = row.split("->");
+      let key_pieces = row_pieces[0].split("(");
+      let node = { "key" : key_pieces[0].replace(/\s/g, ""), "weight" : null, "children" :  (row_pieces[1] ? row_pieces[1].replace(/\s/g, "").split(",") : [])};
+      tower.push(node);
+    });
+
+    for(let x=0; x<tower.length; x++) {
+      root_node = tower[x]["key"];
+      root_node_occurences = 0;
+
+      tower.forEach(function(node2) {
+        if(node2["children"].indexOf(root_node) > -1) {
+          root_node_occurences++;
+        }
+      });
+
+      if(root_node_occurences == 0) {
+        break;
+      }
+    }
+
+    //now I can traverse the tower from the root node to calculate weights of items above
+
+    console.log(root_node);
+  });
+
+})();
