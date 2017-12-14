37c148c395eab133a26185155a59ca3d9c1397bf
Christopher Waite <chrismwaite@gmail.com>
2017-12-14 22:46:12 +0000

    Still working on day 7 part 2 solution

--- day7/solution_part2.js
+++ day7/solution_part2.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input_path = "input_sample.txt";
+const input_path = "input.txt";
 
 function get_parent_node(key, tower) {
   for(let parent_key in tower) {
@@ -45,9 +45,90 @@ function get_parent_node(key, tower) {
       }
     }
 
-    console.log(root_node);
+    //console.log(root_node);
+
+    //console.log(tower);
+
+    for(let key in tower) {
+      //if it has children
+      if(tower[key]["children"]) {
+        let total_weight_counts = {};
+        for(let x=0; x<tower[key]["children"].length; x++) {
+          //each child added to its children must be balanced
+          let child_key = tower[key]["children"][x];
+          let total_weight = parseInt(tower[child_key]["weight"]);
+
+          if(tower[child_key]["children"]) {
+            for(let a=0; a<tower[child_key]["children"].length; a++) {
+              total_weight += parseInt(tower[tower[child_key]["children"][a]]["weight"]);
+            }
+          }
+
+          //total_weight_counts[total_weight] = {};
+          //total_weight_counts[total_weight]["count"] = 1;
+          //total_weight_counts[total_weight]["key"] = child_key;
+
+          if(total_weight_counts[total_weight]) {
+            total_weight_counts[total_weight]["count"] = parseInt(total_weight_counts[total_weight]["count"]) + 1;
+          }
+          else {
+            total_weight_counts[total_weight] = {};
+            total_weight_counts[total_weight]["count"] = 1;
+            total_weight_counts[total_weight]["key"] = child_key;
+          }
+        }
+
+        //console.log(total_weight_counts);
+
+        //this is the one where there is a mismatch in the weights
+        if(Object.keys(total_weight_counts).length > 1) {
+          //console.log(total_weight_counts);
+          //find the one that only has a count of 1, its the odd one out
+          let expected_weight = null;
+          let child_total_weight = null;
+
+          for(let child_weight in total_weight_counts) {
+            if(total_weight_counts[child_weight]["count"] == 1) {
+              incorrect_key = total_weight_counts[child_weight]["key"];
+              child_total_weight = child_weight;
+            }
+            else {
+              expected_weight = child_weight;
+            }
+          }
+          let current_child_weight = parseInt(tower[incorrect_key]["weight"]);
+          //let child_total_weight = parseInt(tower[incorrect_key]["weight"]);
+          
+          if(child_total_weight > expected_weight) {
+            incorrect_key_expected_weight = current_child_weight - (child_total_weight - parseInt(expected_weight));
+          }
+          else {
+            incorrect_key_expected_weight = current_child_weight + (parseInt(expected_weight) - child_total_weight);
+          }
+          console.log("child weight: " + current_child_weight);
+          console.log("child total weight: " + child_total_weight);
+          console.log("child expected total weight: " + expected_weight);
+        }
+
+      }
+      
+      //each child added to its children must be balanced
+      //or each child needs to be balanced if they have no children
 
 
+
+
+      /*if(get_parent_node(key, tower)) {
+        let parent_node = get_parent_node(key, tower);
+        parent_node["total_weight"] = parseInt(parent_node["total_weight"]) + parseInt(tower[key]["weight"]);
+      }*/
+    }
+
+
+
+
+
+    /*
     //foreach node - if it has a parent. add its weight to its total
     for(let key in tower) {
       if(get_parent_node(key, tower)) {
@@ -56,48 +137,60 @@ function get_parent_node(key, tower) {
       }
     }
 
-    
-    console.log(tower);
+    let incorrect_key = null;
+    let incorrect_key_expected_weight = null;
 
     for(let key in tower) {
       //check childrens total weights
       //which one is the odd one out?
       let total_weight_counts = {};
-      for(let x=0; x<tower[key]["children"]; x++) {
-        total_weight_counts[tower[key]["children"][x]] = total_weight_counts[tower[key]["children"][x]] + 1;
+      for(let x=0; x<tower[key]["children"].length; x++) {
+        let child_node = tower[tower[key]["children"][x]];
+        if(total_weight_counts[child_node["total_weight"]]) {
+          total_weight_counts[child_node["total_weight"]]["count"] = parseInt(total_weight_counts[child_node["total_weight"]]["count"]) + 1;
+        }
+        else {
+          total_weight_counts[child_node["total_weight"]] = {};
+          total_weight_counts[child_node["total_weight"]]["count"] = 1;
+          total_weight_counts[child_node["total_weight"]]["key"] = tower[key]["children"][x];
+        }
       }
 
-      
+      //console.log(total_weight_counts);
+
+      //this is the one where there is a mismatch in the weights
+      if(Object.keys(total_weight_counts).length > 1) {
+        console.log(total_weight_counts);
+        //find the one that only has a count of 1, its the odd one out
+        let expected_weight = null;
+        for(let child_weight in total_weight_counts) {
+          if(total_weight_counts[child_weight]["count"] == 1) {
+            incorrect_key = total_weight_counts[child_weight]["key"];
+          }
+          else {
+            expected_weight = tower[total_weight_counts[child_weight]["key"]]["total_weight"];
+          }
+        }
+        let current_child_weight = parseInt(tower[incorrect_key]["weight"]);
+        let child_total_weight = parseInt(tower[incorrect_key]["total_weight"]);
+        
+        if(child_total_weight > expected_weight) {
+          incorrect_key_expected_weight = current_child_weight - (child_total_weight - parseInt(expected_weight));
+        }
+        else {
+          incorrect_key_expected_weight = current_child_weight + (parseInt(expected_weight) - child_total_weight);
+        }
+        //console.log("child weight: " + tower[incorrect_key]["weight"]);
+        //console.log("child total weight: " + tower[incorrect_key]["total_weight"]);
+        //console.log("child expected total weight: " + expected_weight);
+      }
+
 
       //what is the childs weight now? Add the difference between the childs total_weight and what the total_weight should be
-    }
-
-    //check the new tree - which child is the odd one out
-    
-
-
-
-
-
-
-
-
-
-    /*let current_node = tower[root_node];
-    let children = current_node["children"];
-    
-
-    for(let x=0; x<children.length; x++) {
-      let child_node_key = children[x];
-      current_node["total_weight"] = parseInt(current_node["total_weight"]) + parseInt(tower[child_node_key]["weight"]);
-      //current_node = tower[child_node_key];
-    }
-
-    current_node = tower[child_node_key];
-
-    /*while(current_node["children"]) {
-     
     }*/
+
+    console.log(incorrect_key);
+    console.log(incorrect_key_expected_weight);
 
   });
 
